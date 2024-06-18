33ea8871d53115bf86af4fed75c3a51a8f040566
jamsanch26 <jamelasanchez26@gmail.com>
2024-06-18 18:42:54 -0400

    First Commit

---
+++ assignment2/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+     env: process.env.NODE_ENV || 'development', 
+     port: process.env.PORT || 8080,
+     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+     mongoUri: process.env.MONGODB_URI || "mongodb+srv://jamelasanchez26:ZG5sMiyrzg8WtnuT@cluster0.ktsrtd0.mongodb.net/Marketplace?retryWrites=true&w=majority"||
+    process.env.MONGO_HOST ||
+     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+    (process.env.MONGO_PORT || '27017') +
+     '/mernproject' 
+     }
+     export default config
